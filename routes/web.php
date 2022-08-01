a98ac130449f7294fc51b6c2cd8f60479360f8cd
muhammed-ayman <mae252002@gmail.com>
2022-08-01 22:32:21 +0200

    added route arguments

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
     use Psi\Http\Route;
     use App\Controllers\HomeController;
     
+    Route::get('/{name}/{echo}', function($params = []) {
+        
+    });
+
     Route::get('/', [HomeController::class, 'index']);
--- src/Http/Route.php
+++ src/Http/Route.php
@@ -1,6 +1,7 @@
 <?php
 
     namespace Psi\Http;
+    use \Exception;
 
     class Route
     {
@@ -18,11 +19,13 @@
 
         public static function get($route, $action)
         {
+            self::validateRoute($route);    
             self::$routes['get'][$route] = $action;
         }
 
         public static function post($route, $action)
         {
+            self::validateRoute($route);
             self::$routes['post'][$route] = $action;
         }
 
@@ -30,16 +33,55 @@
         {
             $method = $this->request->method();
             $path = $this->request->path();
-            $action = self::$routes[$method][$path] ?? null;
+            $params = array();
+            $action = null;
 
+            foreach (self::$routes[$method] as $route => $value) {
+                if (preg_match($this->generateRouteRegex($route), $path)) {
+                    $params = $this->getRouteParameters($route, $path);
+                    $action = self::$routes[$method][$route];
+                }
+            }
+            
             if (!$action) return;
 
             // Callback Handling
-            if (is_callable($action)) call_user_func_array($action, []);
+            if (is_callable($action)) call_user_func_array($action, [$params]);
             
             // Array Handling
-            if (is_array($action)) call_user_func_array([new $action[0], $action[1]], []);
+            if (is_array($action)) call_user_func_array([new $action[0], $action[1]], $params);
 
             // 404 Handling
+        }
+
+        private function getRouteParameters($route, $path) {
+            $params = array();
+            $exploded_route = explode("/", trim($route, "/"));
+            $path = explode("/", trim($path, "/"));
+
+            for ($i = 0; $i < count($exploded_route); $i++) {
+                if (str_contains($exploded_route[$i], '{')) {
+                    $params[trim($exploded_route[$i], "{}")] = $path[$i];
+                }
+            }
+
+            return $params;
+        }
+
+        private function generateRouteRegex($route) {
+            $route_regex = preg_replace('/{(.*?)}/', '[.~_\d\w-]+', $route);
+            $route_regex = '/^' . str_replace('/', '\/', $route_regex) . '$/';
+            return $route_regex;
+        }
+
+        private static function validateRoute($route) {
+            $route_pattern = '/^\/(?:(?:[.~_\d\w-]+|{[.~_\d\w-]+})(?:\/|$))*$/';
+
+            try {
+                if (!preg_match($route_pattern, $route))
+                    throw new Exception("Invalid Route: $route");
+            } catch(Exception $e) {
+                echo $e->getMessage();
+            }
         }
     }
